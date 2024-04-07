a619bb5e49add19ef059d5062e19696b06c2264a
Abenezer Alebachew Endalew <125548096+abeni-al7@users.noreply.github.com>
2024-04-07 14:41:21 +0300

    Create Remove_Smallest.py

---
+++ Remove_Smallest.py
@@ -0,0 +1,13 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+    pos = True
+    for i in range(n-1):
+        if a[i+1] - a[i] > 1:
+            print("NO")
+            pos = False
+            break
+    if pos:
+        print("YES")
