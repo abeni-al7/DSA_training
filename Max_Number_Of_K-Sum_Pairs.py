acbbc28b9f153d5e10631825c8451eb10a8e3764
Abenezer Alebachew Endalew <125548096+abeni-al7@users.noreply.github.com>
2024-06-13 12:54:35 +0300

    Create Max_Number_Of_K-Sum_Pairs.py

---
+++ Max_Number_Of_K-Sum_Pairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        count = 0
+        l, r = 0, len(nums) - 1
+        while l < r:
+            total = nums[l] + nums[r]
+            if total < k:
+                l += 1
+            elif total > k:
+                r -= 1
+            else:
+                count += 1
+                l += 1
+                r -= 1
+        return count
+
+        
