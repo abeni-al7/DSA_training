1b9912c23b67388258bfd777468fe3ace2b80747
Abenezer Alebachew Endalew <125548096+abeni-al7@users.noreply.github.com>
2024-06-18 16:12:35 +0300

    Create Longest_Repeating_Character_Replacement.py

---
+++ Longest_Repeating_Character_Replacement.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        count = {}
+        l = 0
+        maxLength = 0
+        for r in range(len(s)):
+            count[s[r]] = count.get(s[r], 0) + 1
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] = count.get(s[l], 1) - 1
+                l += 1
+            maxLength = max(r - l + 1, maxLength)
+        return maxLength
+            
