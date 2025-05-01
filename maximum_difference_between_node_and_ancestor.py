71b111661ff6363789d70a748ad33ba97feb92bf
abeni-al7 <abenezeralebachew3@gmail.com>
2025-05-01 16:46:04 +0300

    solve maximum difference between node and ancestor

---
+++ maximum_difference_between_node_and_ancestor.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        def findMax(root, minimum, maximum):
+            if not root:
+                return -1
+            minimum = min(minimum, root.val)
+            maximum = max(maximum, root.val)
+
+            left = findMax(root.left, minimum, maximum)
+            right = findMax(root.right, minimum, maximum)
+
+            return max((maximum - minimum), max(left, right))
+        return findMax(root, float(inf), float(-inf))
