ca59c79d818c6c55ad7c594700587e4dfafbaaf4
Abenezer Alebachew Endalew <125548096+abeni-al7@users.noreply.github.com>
2025-05-02 22:05:56 +0300

    Created 'Remove Smallest' by A2SV Hub

---
+++ 02-May-2025/Remove Smallest 347295.py
@@ -0,0 +1,15 @@
+# Problem: Remove Smallest - https://codeforces.com/problemset/problem/1399/A
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+    pos = True
+    for i in range(n-1):
+        if a[i+1] - a[i] > 1:
+            print("NO")
+            pos = False
+            break
+    if pos:
+        print("YES")
