514a5084a483405f2d3d67eb713b8beabb518f94
abeni-al7 <abenezeralebachew3@gmail.com>
2025-05-03 19:53:30 +0300

    Solve belted rooms

---
+++ belted_rooms.py
@@ -0,0 +1,27 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input()
+    a, b = 0, 0
+    for char in s:
+        if char == ">":
+            a += 1
+        elif char == "<":
+            b += 1
+    if a == 0 or b == 0:
+        print(n)
+    else:
+        total, gap = 0, 0
+        for char in s:
+            if char == "-":
+                total += 1
+            else:
+                if total:
+                    gap += total + 1
+                total = 0
+        if total:
+            gap += total + 1
+        if s[0] == "-" == s[n - 1]:
+            gap -= 1
+        print(gap)
