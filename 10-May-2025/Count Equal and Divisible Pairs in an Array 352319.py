1c4c44bd1034f756d4f0ef4212e9d108cbe85ba5
Abenezer Alebachew Endalew <125548096+abeni-al7@users.noreply.github.com>
2025-05-10 15:12:54 +0300

    Created 'Count Equal and Divisible Pairs in an Array' by A2SV Hub

---
+++ 10-May-2025/Count Equal and Divisible Pairs in an Array 352319.py
@@ -0,0 +1,11 @@
+# Problem: Count Equal and Divisible Pairs in an Array - https://leetcode.com/problems/count-equal-and-divisible-pairs-in-an-array/
+
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        count = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] == nums[j] and (i * j) % k == 0:
+                    count += 1
+        return count
