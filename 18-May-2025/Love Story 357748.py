db731b1d88bed14c202424cd88bb9c816141ad6d
Abenezer Alebachew Endalew <125548096+abeni-al7@users.noreply.github.com>
2025-05-18 14:38:23 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 18-May-2025/Love Story 357748.py
@@ -0,0 +1,12 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+t = int(input())
+
+match = "codeforces"
+for _ in range(t):
+    s = input()
+    count = 0
+    for i in range(10):
+        if s[i] != match[i]:
+            count += 1
+    print(count)
