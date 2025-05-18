01b8c27932331bfc90a0682c97a3060075ab5155
Abenezer Alebachew Endalew <125548096+abeni-al7@users.noreply.github.com>
2025-05-18 14:56:59 +0300

    Created 'Sum' by A2SV Hub

---
+++ 18-May-2025/Sum 357749.py
@@ -0,0 +1,11 @@
+# Problem: Sum - https://codeforces.com/contest/1742/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    a = list(map(int, input().split()))
+    a.sort()
+    if a[0] + a[1] != a[2]:
+        print("NO")
+    else:
+        print("YES")
