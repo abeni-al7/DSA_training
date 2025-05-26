6fac564f86b732697f2f7f5f3ec8b95c56ebd753
Abenezer Alebachew Endalew <125548096+abeni-al7@users.noreply.github.com>
2025-05-26 09:55:32 +0300

    Created 'Majority Element II (Optional)' by A2SV Hub

---
+++ 26-May-2025/Majority Element II (Optional) 359940.py
@@ -0,0 +1,31 @@
+# Problem: Majority Element II (Optional) - https://leetcode.com/problems/majority-element-ii/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+
+        # Step 1: Find up to two candidates
+        candidate1, candidate2 = None, None
+        count1, count2 = 0, 0
+
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        # Step 2: Validate candidates
+        result = []
+        for c in [candidate1, candidate2]:
+            if nums.count(c) > len(nums) // 3:
+                result.append(c)
+    
+        return result
