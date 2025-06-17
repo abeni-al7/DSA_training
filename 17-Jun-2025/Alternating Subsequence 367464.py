d088af92921f610c7389b8fc5d048a7d26ea6ac8
Abenezer Alebachew Endalew <125548096+abeni-al7@users.noreply.github.com>
2025-06-17 14:30:58 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 17-Jun-2025/Alternating Subsequence 367464.py
@@ -0,0 +1,20 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+t = int(input().strip())
+for _ in range(t):
+    n = int(input().strip())
+    a = list(map(int, input().split()))
+    if n == 0:
+        print(0)
+        continue
+    total = 0
+    cur_max = a[0]
+    for i in range(1, n):
+        if (a[i] > 0) == (a[i-1] > 0):
+            if a[i] > cur_max:
+                cur_max = a[i]
+        else:
+            total += cur_max
+            cur_max = a[i] 
+    total += cur_max
+    print(total)
