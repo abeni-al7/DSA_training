168ed1d8d16fc4a2fba82717bd410fd0d0732293
Abenezer Alebachew Endalew <125548096+abeni-al7@users.noreply.github.com>
2025-06-19 20:38:53 +0300

    Created 'Get Equal Substrings Within Budget' by A2SV Hub

---
+++ 19-Jun-2025/Get Equal Substrings Within Budget 368158.py
@@ -0,0 +1,14 @@
+# Problem: Get Equal Substrings Within Budget - https://leetcode.com/problems/get-equal-substrings-within-budget/
+
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        max_len = 0
+        l = 0
+        current_cost = 0
+        for r in range(len(s)):
+            current_cost += abs(ord(s[r]) - ord(t[r]))
+            while current_cost > maxCost:
+                current_cost -= abs(ord(s[l]) - ord(t[l]))
+                l += 1
+            max_len = max(max_len, r - l + 1)
+        return max_len
