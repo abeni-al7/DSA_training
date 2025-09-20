54ada8c007929e04506e12b14d68718a4068e8e4
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-09-20 20:49:46 +0300

    Created 'Evaluate Reverse Polish Notation' by A2SV Hub

---
+++ 20-Sep-2025/Evaluate Reverse Polish Notation 394485.py
@@ -0,0 +1,19 @@
+# Problem: Evaluate Reverse Polish Notation - https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        ops = {
+            "+": operator.add,
+            "-": operator.sub,
+            "*": operator.mul,
+            "/": lambda x, y: int(operator.truediv(x, y))
+        }
+
+        for token in tokens:
+            if token in ops:
+                b, a = stack.pop(), stack.pop()
+                stack.append(ops[token](a, b))
+            else:
+                stack.append(int(token))
+        return stack[0]
