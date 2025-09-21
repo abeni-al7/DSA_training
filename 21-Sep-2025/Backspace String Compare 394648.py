3a28034e2949fc63f3234d46d3f063deba8a3dc5
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-09-21 17:21:20 +0300

    Created 'Backspace String Compare' by A2SV Hub

---
+++ 21-Sep-2025/Backspace String Compare 394648.py
@@ -0,0 +1,20 @@
+# Problem: Backspace String Compare - https://leetcode.com/problems/backspace-string-compare/
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        s_stack = []
+        t_stack = []
+        for char in s:
+            if s_stack and char == "#":
+                s_stack.pop()
+            elif char != "#":
+                s_stack.append(char)
+        
+        for char in t:
+            if t_stack and char == "#":
+                t_stack.pop()
+            elif char != "#":
+                t_stack.append(char)
+        
+        return s_stack == t_stack
+        
