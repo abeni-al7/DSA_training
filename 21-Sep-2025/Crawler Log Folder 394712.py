c28ad74adfb3b753c7276a8e7f46f318ebfa3216
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-09-21 20:47:04 +0300

    Created 'Crawler Log Folder' by A2SV Hub

---
+++ 21-Sep-2025/Crawler Log Folder 394712.py
@@ -0,0 +1,16 @@
+# Problem: Crawler Log Folder - https://leetcode.com/problems/crawler-log-folder/
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+        
+        for log in logs:
+            if log == "../" and stack:
+                stack.pop()
+            elif log == "./" or (log == "../" and not stack):
+                continue
+            else:
+                stack.append(log)
+      
+        return len(stack)
+        
