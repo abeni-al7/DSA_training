4d3bce32c800754aa6147e807fc30f5bdc113118
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-09-22 17:49:31 +0300

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 22-Sep-2025/Fibonacci Number 395016.py
@@ -0,0 +1,11 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def __init__(self):
+        self.memo = [0] * 31
+        self.memo[1] = 1
+
+    def fib(self, n: int) -> int:
+        if n not in self.memo:
+            self.memo[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.memo[n]
