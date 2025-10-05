3967fa2197f79ecadd0338fb9a922496a4253264
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-10-05 18:21:54 +0300

    Created 'Word Break' by A2SV Hub

---
+++ 05-Oct-2025/Word Break 399980.py
@@ -0,0 +1,15 @@
+# Problem: Word Break - https://leetcode.com/problems/word-break/description/
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = [False] * (len(s) + 1)
+        memo[len(s)] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if i + len(w) <= len(s) and s[i: i + len(w)] == w:
+                    memo[i] = memo[i + len(w)]
+                if memo[i]:
+                    break
+        
+        return memo[0]
