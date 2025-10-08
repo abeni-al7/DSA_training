ba1f04688f6d1e197ac837539621ae732131ce7b
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-10-08 09:51:11 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 08-Oct-2025/Almost Prime 400850.py
@@ -0,0 +1,22 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+def count_factors(n):
+  d = 2
+  num = n
+  factors = set()
+
+  while d * d <= n:
+    while num % d == 0:
+      num //= d
+      factors.add(d)
+    d += 1
+  if num > 1:
+    factors.add(num)
+  return len(factors) == 2
+
+n = int(input())
+count = 0
+for i in range(1, n+1):
+  if count_factors(i):
+    count += 1
+print(count)
