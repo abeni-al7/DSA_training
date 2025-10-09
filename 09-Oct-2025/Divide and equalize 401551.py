7873eec808b47a8ed7056859ff232b1877d10fc6
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-10-09 19:27:00 +0300

    Created 'Divide and equalize' by A2SV Hub

---
+++ 09-Oct-2025/Divide and equalize 401551.py
@@ -0,0 +1,30 @@
+# Problem: Divide and equalize - https://codeforces.com/problemset/problem/1881/D
+
+from collections import defaultdict
+
+t = int(input())
+
+def modify_map(n, primes):
+    d = 2
+    while d * d <= n:
+        while n % d == 0:
+            primes[d] += 1
+            n //= d
+        d += 1
+    if n > 1:
+        primes[n] += 1
+
+for _ in range(t):
+    n = int(input())
+    nums = list(map(int, input().split()))
+
+    primes = defaultdict(int)
+    for num in nums:
+        modify_map(num, primes)
+    
+    for val in primes.values():
+        if val % n != 0:
+            print("NO")
+            break
+    else:
+        print("YES")
