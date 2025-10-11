15e632f0b0280c32aaa09765d89ec75d6283ba8e
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-10-11 17:16:17 +0300

    Created 'Row GCD' by A2SV Hub

---
+++ 11-Oct-2025/Row GCD 402137.py
@@ -0,0 +1,27 @@
+# Problem: Row GCD - https://codeforces.com/problemset/problem/1458/a
+
+n, m = map(int, input().split())
+first = list(map(int, input().split()))
+second = list(map(int, input().split()))
+
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+result = []
+if n == 1:
+    for num in second:
+        result.append(first[0] + num)
+else:
+    g = 0
+    for i in range(1, n):
+        val = first[i] - first[0]
+        g = gcd(g, val)
+
+    for num in second:
+        result.append(abs(gcd(first[0] + num, g)))
+
+print(*result)
