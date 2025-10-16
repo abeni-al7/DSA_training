26690a6d141ad49dfabf95370874a5c961c9fdef
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-10-16 17:43:27 +0300

    Created 'Dreamoon and Stairs' by A2SV Hub

---
+++ 16-Oct-2025/Dreamoon and Stairs 403721.py
@@ -0,0 +1,17 @@
+# Problem: Dreamoon and Stairs - https://codeforces.com/problemset/problem/476/A
+
+n, m = map(int, input().split())
+
+if n < m:
+    print(-1)
+elif n == m:
+    print(n)
+else:
+    if n % 2 == 0:
+        curr = n // 2
+    else:
+        curr = (n // 2) + 1
+
+    while curr % m != 0:
+        curr += 1
+    print(curr)   
