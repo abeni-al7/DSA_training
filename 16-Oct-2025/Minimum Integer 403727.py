77e6269cd40964e08fad42d027ee097f8d7e61fc
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-10-16 17:57:28 +0300

    Created 'Minimum Integer' by A2SV Hub

---
+++ 16-Oct-2025/Minimum Integer 403727.py
@@ -0,0 +1,10 @@
+# Problem: Minimum Integer - https://codeforces.com/problemset/problem/1101/A
+
+q = int(input())
+
+for _ in range(q):
+    l, r, d = map(int, input().split())
+    if d < l or d > r:
+        print(d)
+    else:
+        print(((r // d) * d) + d)
