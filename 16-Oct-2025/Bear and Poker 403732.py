0f8e577728eaf8f1cdd25d1b3bc3675a19705df7
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-10-16 18:26:20 +0300

    Created 'Bear and Poker' by A2SV Hub

---
+++ 16-Oct-2025/Bear and Poker 403732.py
@@ -0,0 +1,21 @@
+# Problem: Bear and Poker - https://codeforces.com/problemset/problem/574/C
+
+n = int(input())
+a = list(map(int, input().split()))
+
+curr = a[0]
+while curr % 2 == 0:
+    curr //= 2
+while curr % 3 == 0:
+    curr //= 3
+
+for num in a:
+    while num % 2 == 0:
+        num //= 2
+    while num % 3 == 0:
+        num //= 3
+    if num != curr:
+        print("No")
+        break
+else:
+    print("Yes")
