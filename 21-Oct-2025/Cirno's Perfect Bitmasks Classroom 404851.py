3755c42842702d7e354d4ca697eece0748b3adaf
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-10-21 16:57:14 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 21-Oct-2025/Cirno's Perfect Bitmasks Classroom 404851.py
@@ -0,0 +1,26 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t = int(input())
+
+for _ in range(t):
+    x = int(input())
+    y = 0
+
+    and_valid = False
+    xor_valid = False
+    xor_modified = -1
+    for i in range(32):
+        if (x >> i) & 1 and not and_valid:
+            y |= 1 << i
+            and_valid = True
+            and_modified = i
+        elif (x >> i) & 1:
+            if xor_valid:
+                y &= ~(1 << xor_modified)
+            xor_valid = True
+            xor_modified = i
+        elif not (x >> i) & 1 and not xor_valid:
+            y |= 1 << i
+            xor_valid = True
+            xor_modified = i
+    print(y)
