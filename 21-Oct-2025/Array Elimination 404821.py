8b7d5e04793eb33b62de3a1fa66c88a88748e98a
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-10-21 13:32:48 +0300

    Created 'Array Elimination' by A2SV Hub

---
+++ 21-Oct-2025/Array Elimination 404821.py
@@ -0,0 +1,23 @@
+# Problem: Array Elimination - https://codeforces.com/contest/1601/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    pos_count = [0] * 32
+    for i in range(32):
+        for num in a:
+            if num & (1 << i):
+                pos_count[i] += 1
+    
+    ans = [1]
+    for i in range(1, n):
+        ind = i + 1
+        for val in pos_count:
+            if val % ind != 0:
+                break
+        else:
+            ans.append(ind)
+    print(*ans)
