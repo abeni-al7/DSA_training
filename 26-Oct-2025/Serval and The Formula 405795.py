cc96b599dbd4c6c1c3b2025abaa0cf64af61a4d5
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-10-26 14:18:59 +0300

    Created 'Serval and The Formula' by A2SV Hub

---
+++ 26-Oct-2025/Serval and The Formula 405795.py
@@ -0,0 +1,10 @@
+# Problem: Serval and The Formula - https://codeforces.com/problemset/problem/2085/C
+
+t = int(input())
+
+for _ in range(t):
+    x, y = map(int, input().split())
+    if x == y:
+        print(-1)
+    else:
+        print((1 << 49) - max(x, y))
