f8bb4c9d4dd24916e9082f479fbee31ac984bd30
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-11-04 18:01:45 +0300

    Created 'Dictionary' by A2SV Hub

---
+++ 04-Nov-2025/Dictionary 407348.py
@@ -0,0 +1,13 @@
+# Problem: Dictionary - https://codeforces.com/problemset/problem/1674/B
+
+t = int(input())
+for _ in range(t):
+    word = input()
+    first = ord(word[0]) - ord("a")
+    second = ord(word[1]) - ord("a")
+    index = (first * 25)
+    if second > first:
+        index += second
+    else:
+        index += second + 1
+    print(index)
