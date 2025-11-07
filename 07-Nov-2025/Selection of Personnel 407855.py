29a20d118b9eb64a24d448212f34a709d86212fe
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-11-07 18:21:04 +0300

    Created 'Selection of Personnel' by A2SV Hub

---
+++ 07-Nov-2025/Selection of Personnel 407855.py
@@ -0,0 +1,15 @@
+# Problem: Selection of Personnel - https://codeforces.com/problemset/problem/630/F
+
+n = int(input())
+
+fact = [0] * (n + 1)
+fact[0] = fact[1] = 1
+
+for i in range(2, n + 1):
+    fact[i] = i * fact[i - 1]
+
+group5 = (fact[n]) // (fact[n - 5] * fact[5])
+group6 = (fact[n]) // (fact[n - 6] * fact[6])
+group7 = (fact[n]) // (fact[n - 7] * fact[7])
+
+print(group5 + group6 + group7)
