d51918bf24953d9bed89a853aeb3a23074ab3d4c
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-11-07 19:38:10 +0300

    Created 'k-Factorization' by A2SV Hub

---
+++ 07-Nov-2025/k-Factorization 407883.py
@@ -0,0 +1,20 @@
+# Problem: k-Factorization - https://codeforces.com/problemset/problem/797/A
+
+n, k = map(int, input().split())
+i = 2
+
+vals = []
+while n > 1 and k > 1:
+    while n % i:
+        i += 1
+    n //= i
+    vals.append(i)
+    k -= 1
+if n > 1:
+    vals.append(n)
+    k -= 1
+
+if k > 0:
+    print(-1)
+else:
+    print(*vals)
