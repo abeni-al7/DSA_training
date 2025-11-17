ed578554b848413527558591be6e079ed61137ff
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2025-11-17 19:57:55 +0300

    Created 'People are leaving' by A2SV Hub

---
+++ 17-Nov-2025/People are leaving 409159.py
@@ -0,0 +1,29 @@
+# Problem: People are leaving - https://codeforces.com/edu/course/2/lesson/7/2/practice/contest/289391/problem/A
+
+n, m = map(int, input().split())
+res = []
+parent = {i: i for i in range(n + 2)}
+
+
+def find(x):
+    path = []
+    while parent[x] != x:
+        path.append(x)
+        x = parent[x]
+    for node in path:
+        parent[node] = x
+    return x 
+
+
+for _ in range(m):
+    act, node = input().split()
+    node = int(node)
+    if act == "-":
+        parent[node] = find(node + 1)
+    else:
+        res.append(find(node))
+
+for item in res:
+    if item == n + 1:
+        item = -1
+    print(item)
