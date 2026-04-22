4ff571957483d64446715cca84ddd2203c79f583
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2026-04-22 12:39:56 +0300

    0295-FIND-MEDIAN-FROM-DATA-STREAM Stats: Time: 231 ms (19.31%), Space: 42.2 MB (36.09%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -6,21 +6,15 @@ class MedianFinder:
 
     def addNum(self, num: int) -> None:
         heapq.heappush_max(self.smallHeap, num)
-        if len(self.smallHeap) > len(self.bigHeap) + 1:
-            heapq.heappush(self.bigHeap, heapq.heappop_max(self.smallHeap))
-        
-        if self.smallHeap and self.bigHeap and self.smallHeap[0] > self.bigHeap[0]:
-            heapq.heappush(self.bigHeap, heapq.heappop_max(self.smallHeap))
-        
+        heapq.heappush(self.bigHeap, heapq.heappop_max(self.smallHeap))
+
         if len(self.bigHeap) > len(self.smallHeap) + 1:
             heapq.heappush_max(self.smallHeap, heapq.heappop(self.bigHeap))
 
     def findMedian(self) -> float:
-        if len(self.smallHeap) == len(self.bigHeap):
-            return (self.smallHeap[0] + self.bigHeap[0]) / 2
-        elif len(self.smallHeap) > len(self.bigHeap):
-            return self.smallHeap[0]
-        return self.bigHeap[0]
+        if len(self.bigHeap) > len(self.smallHeap):
+            return self.bigHeap[0]
+        return (self.bigHeap[0] + self.smallHeap[0]) / 2
         
 
 
