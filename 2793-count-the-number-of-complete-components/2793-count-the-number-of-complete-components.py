99e6c30eb6dbe859500bf7ec0c61801a35d15608
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2026-07-11 19:55:27 +0300

    2793-COUNT-THE-NUMBER-OF-COMPLETE-COMPONENTS Stats: Time: 37 ms (70.03%), Space: 21.6 MB (17.7%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
@@ -1,21 +1,30 @@
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
-        graph = [[] for _ in range(n)]
-        componentFreq = defaultdict(int)
+        graph = defaultdict(list)
 
-        for i in range(n):
-            graph[i].append(i)
-        
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
-        
-        for i in range(n):
-            graph[i].sort()
-            componentFreq[tuple(graph[i])] += 1
-        
+
         completeCount = 0
-        for k, v in componentFreq.items():
-            if len(k) == v:
+        visited = set()
+
+        def dfs(curr, info):
+            visited.add(curr)
+            info[0] += 1
+            info[1] += len(graph[curr])
+
+            for child in graph[curr]:
+                if child not in visited:
+                    dfs(child, info)
+            
+        for i in range(n):
+            if i in visited:
+                continue
+            
+            componentInfo = [0, 0]
+            dfs(i, componentInfo)
+
+            if componentInfo[0] * (componentInfo[0] - 1) == componentInfo[1]:
                 completeCount += 1
         return completeCount
