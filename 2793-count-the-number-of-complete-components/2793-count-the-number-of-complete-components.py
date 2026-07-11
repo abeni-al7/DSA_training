b2b04a9c0a4daaf8df18fb415660d0ac4011f035
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2026-07-11 19:32:35 +0300

    2793-COUNT-THE-NUMBER-OF-COMPLETE-COMPONENTS Stats: Time: 36 ms (72.2%), Space: 19.8 MB (71.89%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
@@ -1,47 +1,21 @@
-class UnionFind:
-    def __init__(self, n):
-        self.parent = [i for i in range(n)]
-        self.size = [1 for _ in range(n)]
-    
-    def find(self, x):
-        parent = self.parent[x]
-        if parent != self.parent[parent]:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        parX, parY = self.find(x), self.find(y)
-        if self.size[parX] >= self.size[parY]:
-            self.parent[parY] = parX
-            self.size[parX] += self.size[parY]
-        else:
-            self.parent[parX] = parY
-            self.size[parY] += self.size[parX]
-    
-    def components(self):
-        components = defaultdict(list)
-        for i, v in enumerate(self.parent):
-            components[v].append(i)
-        return components
-
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
-        uf = UnionFind(n)
-        graph = defaultdict(list)
-        for a, b in edges:
-            graph[a].append(b)
-            graph[b].append(a)
-            uf.union(a, b)
+        graph = [[] for _ in range(n)]
+        componentFreq = defaultdict(int)
+
+        for i in range(n):
+            graph[i].append(i)
         
-        count = 0
-        components = uf.components()
-        for comp, items in components.items():
-            size = len(items)
-            complete = True
-            for node in items:
-                if len(graph[node]) != size - 1:
-                    complete = False
-                    break
-            if complete:
-                count += 1
-        return count
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        for i in range(n):
+            graph[i].sort()
+            componentFreq[tuple(graph[i])] += 1
+        
+        completeCount = 0
+        for k, v in componentFreq.items():
+            if len(k) == v:
+                completeCount += 1
+        return completeCount
