e02a9a563ca1e94754ec4ad19be99542c356e88b
Abenezer Alebachew Endalew <abenezeralebachew3@gmail.com>
2026-07-12 16:49:35 +0300

    1256-RANK-TRANSFORM-OF-AN-ARRAY Stats: Time: 55 ms (29.3%), Space: 37.5 MB (60.61%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -1,16 +1,12 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        heap = []
-        for i, num in enumerate(arr):
-            heapq.heappush(heap, (num, i))
-        
-        res = [0] * len(arr)
-        rank = 0
-        prev = -1
-        while heap:
-            curr, idx = heapq.heappop(heap)
-            if prev < 0 or arr[prev] != arr[idx]:
+        num_to_rank = {}
+        sorted_arr = sorted(arr)
+        rank = 1
+        for i in range(len(sorted_arr)):
+            if i > 0 and sorted_arr[i] > sorted_arr[i - 1]:
                 rank += 1
-            res[idx] = rank
-            prev = idx
-        return res
+            num_to_rank[sorted_arr[i]] = rank
+        for i in range(len(arr)):
+            arr[i] = num_to_rank[arr[i]]
+        return arr
